ef4833d8efd113a30965f621664c7e75e9d3237d
Miquel Puig <mpuig22@gmail.com>
2019-05-13 18:00:23 +0200

    done

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, NgModule } from '@angular/core';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styles: []
 })
 export class TemplateComponent implements OnInit {
-
+  data="Hola";
   constructor() { }
 
   ngOnInit() {
+  }
+  guardar( forma: any ){
+    console.log("Formulario Posteado");
+    console.log(forma);
   }
 
 }
