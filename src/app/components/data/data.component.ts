3ba9dcb7aac74cb05aca1fc48a61c44008606ddc
Miquel Puig Gibert <miquel@MacBookdeMiquel.home>
2019-05-19 23:13:57 +0200

    data

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class DataComponent  {
+  usuario: Object = {
+    nombrecompleto: {
+      nombre: "Fernando",
+      apellido: "Herrera"
+    },
+    correo: "fernando.herrera85@gmail.com"
   }
+
+  forma: FormGroup;
+  
+  constructor() { 
+    console.log(this.usuario);
+    this.forma = new FormGroup({
+      'nombrecompleto': new FormGroup({
+          'nombre': new FormControl('Fernadno', [Validators.required,Validators.minLength(5)]),
+          'apellido': new FormControl('', Validators.required)
+    }),
+      'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
+    });
+
+  }
+
+  guardarCambios(){
+    console.log(this.forma.value);
+    console.log(this.forma);
+  }
+  
 
 }
