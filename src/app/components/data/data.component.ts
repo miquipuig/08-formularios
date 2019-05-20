60c54d56bfab59e0857fc03f0f4818ecce5f2da7
Miquel Puig <mpuig22@gmail.com>
2019-05-21 00:16:11 +0200

    bind

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -22,17 +22,19 @@ export class DataComponent  {
     console.log(this.usuario);
     this.forma = new FormGroup({
       'nombrecompleto': new FormGroup({
-          'nombre': new FormControl('Fernadno', [Validators.required,Validators.minLength(5)]),
-          'apellido': new FormControl('', Validators.required)
+          'nombre': new FormControl('Fernando', [Validators.required,Validators.minLength(5)]),
+          'apellido': new FormControl('', [Validators.required, this.noHerrera])
     }),
       'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
       'pasatiempos': new FormArray([
         new FormControl('Correr', Validators.required)
-      ])
+      ]),
+      'password1' : new FormControl('', Validators.required),
+      'password2' : new FormControl(),
     });
 
   // this.forma.setValue(this.usuario);
-
+    this.forma.controls['password2'].setValidators([this.noIgual.bind(this.forma), Validators.required]);
   }
 
   agregarPasatiempo() {
@@ -40,20 +42,44 @@ export class DataComponent  {
           new FormControl('', Validators.required)
       )
   }
+  
+  noHerrera( control: FormControl ): { [s: string]: boolean } {
+  if (control.value === "herrera") {
+    return {
+      noherrera: true
+    }
+  }
+  return null;
+  }
+
+  noIgual( control: FormControl ): { [s: string]: boolean } {
+    let forma: any = this;
+    if (control.value !== forma.controls['password1'].value) {
+      return {
+        noiguales: true
+      }
+    }
+    return null;
+  }
+  
+
 
   guardarCambios(){
     console.log(this.forma.value);
     console.log(this.forma);
     // this.forma.reset(this.usuario);
-    this.forma.reset({
-      nombrecompleto: {
-        nombre: '',
-        apellido: ''
-      },
-      correo: ''
-
-    });
     // this.forma.controls['correo'].setValue('nuevocorreo@asasasdads.com');
-  }
+
+    // this.forma.reset({
+    //   nombrecompleto: {
+    //     nombre: '',
+    //     apellido: ''
+    //   },
+    //   correo: ''
+
+    // });
+  
+
+    }
 
 }
